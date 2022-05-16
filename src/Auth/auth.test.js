7138d8ac40ce0505341e8d4d1587abacdce0d532
rizwanNode <102730752+rizwanNode@users.noreply.github.com>
2022-05-16 19:36:34 +0500

    Update auth.test.js

--- src/Auth/auth.test.js
+++ src/Auth/auth.test.js
@@ -2,10 +2,10 @@ import AUTH_CONFIG from './auth0-variables';
 
 describe('Auth Variables Test', () => {
   it('Callback URL should point to the recruiter domain', () => {
-    expect(AUTH_CONFIG.callbackUrl).toEqual('https://recruiter.deephire.com/user/callback');
+    expect(AUTH_CONFIG.callbackUrl).toEqual('https://recruiter.jurbly.com/user/callback');
   });
 
   it('Callback URL should point to the recruiter domain', () => {
-    expect(AUTH_CONFIG.domain).toEqual('login.deephire.com');
+    expect(AUTH_CONFIG.domain).toEqual('login.jurbly.com');
   });
 });
