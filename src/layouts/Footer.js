97046e6686dad88c96de88e113e0767f7ee11c18
rizwanNode <102730752+rizwanNode@users.noreply.github.com>
2022-05-17 02:25:10 +0500

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -5,7 +5,7 @@ import GlobalFooter from '@/components/GlobalFooter';
 const { Footer } = Layout;
 const FooterView = () => (
   <Footer style={{ padding: 0 }}>
-    <GlobalFooter copyright={<Fragment>Powered by DeepHire | Find your fit.</Fragment>} />
+    <GlobalFooter copyright={<Fragment>Powered by Jurbly | Find your fit.</Fragment>} />
   </Footer>
 );
 export default FooterView;
