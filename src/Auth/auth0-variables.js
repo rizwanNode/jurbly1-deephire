dc231039818fc18bf21e0c3b8c078670e0d53413
rizwanNode <102730752+rizwanNode@users.noreply.github.com>
2022-05-17 17:09:01 +0500

    Update auth0-variables.js

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 const currentUrl = `${window.location.origin}/user/callback`;
 
 const AUTH_CONFIG = {
-  domain: 'jurbly.com',
+  domain: 'dev--sdco1d1.us.auth0.com',
   clientId: '4iZ4LA4Xyw8xKvjmU3oHdWtIz1TQhmgB',
   callbackUrl: currentUrl,
   // callbackUrl: 'http://localhost:8000/user/callback',
