941c887e1060853f9440c9a7394a14cc228fb8b9
Rizwan Ahmad <rizvi98ahmad@gmail.com>
2022-06-08 12:44:23 +0500

    icons 45

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -4,8 +4,8 @@ const AUTH_CONFIG = {
   domain: 'dev--sdco1d1.us.auth0.com',
   clientId: '4iZ4LA4Xyw8xKvjmU3oHdWtIz1TQhmgB',
   // callbackUrl: currentUrl,
-  callbackUrl: 'http://localhost:8000/user/callback',
-  // callbackUrl: 'https://recruiter.jurbly.com/user/callback',
+  // callbackUrl: 'http://localhost:8000/user/callback',
+  callbackUrl: 'https://recruiter.jurbly.com/user/callback',
   dbConnectionName: 'JurblyDB',
 };
 
