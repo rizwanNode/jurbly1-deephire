26f61af3e6159e7154525da883a4ad82e090da61
Rizwan Ahmad <rizvi98ahmad@gmail.com>
2022-06-16 14:13:10 +0500

    update 1

--- src/pages/Billing/BillingCards.js
+++ src/pages/Billing/BillingCards.js
@@ -13,7 +13,7 @@ const data = [
     priceLabel: 'By Default No Cost',
     type: 'purchase',
 
-    stripePlan: 'price_1L1TacHgqvk3POlbO6ypMkoZ',
+    stripePlan: 'price_1LBDT5Hgqvk3POlbD5g4LjJL',
 
     listItems: [
       {
@@ -45,7 +45,7 @@ const data = [
     type: 'purchase',
 
     buttonLabel: 'Start Now',
-    stripePlan: 'price_1L8aIgHgqvk3POlbvygHpAYa',
+    stripePlan: 'price_1LBDSMHgqvk3POlbWke9hVsQ',
 
     listItems: [
       {
